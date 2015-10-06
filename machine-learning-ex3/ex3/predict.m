2f9fb2522da4160dd1e578f64c5f3ec5006b75f5
Brandon Gay <brandoncgay@gmail.com>
2015-10-06 16:14:39 -0700

    exercise 3 complete

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -22,8 +22,18 @@ p = zeros(size(X, 1), 1);
 %
 
 
+% Add ones to the data matrix
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
 
+% Add ones to the data matrix
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+[prob, i] = max(a3, [], 2);
+p = i;
 
 
 
